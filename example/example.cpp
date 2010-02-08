bef0ca17d009ec08ea801a9a273d6cc993495d81
Matthias Bach <marix@marix.org>
2010-02-08 18:02:53 +0100

    Adjusted example to the latest revision of the standard.

--- example/example.cpp
+++ example/example.cpp
@@ -16,12 +16,57 @@ using namespace std;
 
 int main( int, char** )
 {
-	char chBuffer[ 256 ];
+	const cl_uint MAX_PLATFORMS = 256;
+	cl_int err;
+	
+	cl_platform_id platforms[ MAX_PLATFORMS ];
+	cl_uint nPlatforms;
+		
+	err = clGetPlatformIDs( MAX_PLATFORMS, platforms, &nPlatforms );
+	if( err )
+	{
+		cerr << "Failed to get platforms" << endl;
+		return -1;
+	}
 
-	// Get and log the platform info
-	clGetPlatformInfo( 0, CL_PLATFORM_VERSION, sizeof(chBuffer), chBuffer, NULL);
+	for( int i = 0; i < nPlatforms; ++i )
+	{ 
+		char chBuffer[ 256 ];
+	
+		// Get and log the platform info
+		err = clGetPlatformInfo( platforms[ i ], CL_PLATFORM_NAME, sizeof(chBuffer), chBuffer, NULL);
+		if( err ) 
+		{
+			cerr << "Failed to get platform version" << endl;
+			return -1;
+		}
+		cout << chBuffer << " - ";
 
-	cout << chBuffer << endl;
+		// Get and log the platform info
+		err = clGetPlatformInfo( platforms[ i ], CL_PLATFORM_VERSION, sizeof(chBuffer), chBuffer, NULL);
+		if( err ) 
+		{
+			cerr << "Failed to get platform version" << endl;
+			return -1;
+		}
+		cout << chBuffer << " - ";
+	
+		err = clGetPlatformInfo( platforms[ i ], CL_PLATFORM_VENDOR, sizeof(chBuffer), chBuffer, NULL);
+		if( err ) 
+		{
+			cerr << "Failed to get platform version" << endl;
+			return -1;
+		}
+		cout << chBuffer << " - ";
+
+		err = clGetPlatformInfo( platforms[ i ], CL_PLATFORM_PROFILE, sizeof(chBuffer), chBuffer, NULL);
+		if( err ) 
+		{
+			cerr << "Failed to get platform profile" << endl;
+			return -1;
+		}
+		cout << chBuffer << endl;
+	}
 
 	return 0;
 }

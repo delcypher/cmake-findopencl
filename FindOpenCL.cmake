6b115f8f16ecec6f5f00c04e30da60647cabc2ac
Matthias Bach <marix@marix.org>
2010-02-15 14:51:56 +0100

    Added detection for CPP Bindings

--- FindOpenCL.cmake
+++ FindOpenCL.cmake
@@ -20,12 +20,14 @@ IF (APPLE)
 
   FIND_LIBRARY(OPENCL_LIBRARIES OpenCL DOC "OpenCL lib for OSX")
   FIND_PATH(OPENCL_INCLUDE_DIRS OpenCL/cl.h DOC "Include for OpenCL on OSX")
+  FIND_PATH(_OPENCL_CPP_INCLUDE_DIRS OpenCL/cl.hpp DOC "Include for OpenCL CPP bindings on OSX")
 
 ELSE (APPLE)
 
 	IF (WIN32)
 	
 	    FIND_PATH(OPENCL_INCLUDE_DIRS CL/cl.h)
+	    FIND_PATH(_OPENCL_CPP_INCLUDE_DIRS CL/cl.hpp)
 	
 	    # TODO this is only a hack assuming the 64 bit library will
 	    # not be found on 32 bit system
@@ -48,12 +50,20 @@ ELSE (APPLE)
             # in /usr/include, therefore also search relative
             # to the library
             FIND_PATH(OPENCL_INCLUDE_DIRS CL/cl.h PATHS ${_OPENCL_INC_CAND})
+            FIND_PATH(_OPENCL_CPP_INCLUDE_DIRS CL/cl.hpp PATHS ${_OPENCL_INC_CAND})
 
 	ENDIF (WIN32)
 
 ENDIF (APPLE)
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS( OpenCL DEFAULT_MSG OPENCL_LIBRARIES OPENCL_INCLUDE_DIRS )
+
+IF( _OPENCL_CPP_INCLUDE_DIRS )
+	SET( OPENCL_HAS_CPP_BINDINGS TRUE )
+	LIST( APPEND OPENCL_INCLUDE_DIRS ${_OPENCL_CPP_INCLUDE_DIRS} )
+	# This is often the same, so clean up
+	LIST( REMOVE_DUPLICATES OPENCL_INCLUDE_DIRS )
+ENDIF( _OPENCL_CPP_INCLUDE_DIRS )
 
 MARK_AS_ADVANCED(
   OPENCL_INCLUDE_DIRS
